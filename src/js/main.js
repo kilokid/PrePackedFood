33db0d00fb408542f9e7d6bc5e57e9043bd5ef77
kilokid <arlomov007@gmail.com>
2021-12-16 14:01:30 +0300

    feat: retrieving data from a database

--- src/js/main.js
+++ src/js/main.js
@@ -264,4 +264,8 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     }
+
+    fetch('http://localhost:3000/menu')
+        .then((data) => data.json())
+        .then((res) => console.log(res));
 });
